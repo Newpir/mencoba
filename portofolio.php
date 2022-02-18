7f10a294dd1289aaa0420b0fdf887935d38b4b11
Newpir <savirauliya10@gmail.com>
2022-02-18 13:53:21 +0700

    penghapusan kata

--- portofolio.php
+++ portofolio.php
@@ -13,7 +13,7 @@
 
 <body>
 	<?php include "navbar.php" ?>
-	<h2 style="text-align: center;">List Biodata </h2>
+	<h2 style="text-align: center;">List Biodata</h2>
 	<div class="porto-content">
 		<?php for($i = 0; $i < 10; $i++) { ?>
 			<div class="porto-item">
@@ -22,9 +22,7 @@
 					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
 					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
 					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+					consequat.
 				</div>
 				<a class="button" href="#detail">Detail</a>
 			</div>
